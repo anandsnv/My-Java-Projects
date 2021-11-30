a474611b97e4bdb84d72a729ef90a30de35e2203
anandsnv <anand.s@simelabs.com>
2021-11-30 21:50:23 +0530

    C#

---
+++ Array c#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            bool[] flags = new bool[3]; 
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+        }
+    }
+}

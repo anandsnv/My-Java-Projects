8c7a459f535d7c86e41404668a92251c77efe577
anandsnv <anand.s@simelabs.com>
2022-01-03 16:21:55 +0530

    Final

---
+++ ResumeManager/ResumeManager/Controllers/ResumeController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ResumeManager.Data;
+using ResumeManager.Models;
+
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ResumeManager.Controllers
+{
+    public class ResumeController : Controller
+    {
+        private readonly ResumeDbContext _context;
+
+        private readonly IWebHostEnvironment _webHost;
+
+
+
+
+        public ResumeController(ResumeDbContext context, IWebHostEnvironment webHost)
+        {
+            _context = context;
+            _webHost = webHost;
+
+        }
+        public IActionResult Index(string SearchText = "")
+        {
+            List<Applicant> applicants;
+
+            if (SearchText != "" && SearchText != null)
+            {
+                applicants = _context.Applicants
+                .Include(e => e.Experiences)
+                .Where(a => a.Name.Contains(SearchText) || a.Qualification.Contains(SearchText) || a.Gender.Contains(SearchText))
+                .ToList();
+                 return View(applicants);    
+            }
+            else
+            {
+                applicants = _context.Applicants.ToList();
+                return View(applicants);
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Applicant applicant = new Applicant();
+            applicant.Experiences.Add(new Experience() { ExperienceId = 1 });
+        
+            return View(applicant);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Applicant applicant)
+        {
+            applicant.Experiences.RemoveAll(n => n.YearsWorked == 0);
+
+            string uniqueFileName = GetUploadedFileName(applicant);
+            applicant.PhotoUrl = uniqueFileName;
+
+            _context.Add(applicant);
+            _context.SaveChanges();
+            return RedirectToAction("index");
+
+        }
+
+
+        private string GetUploadedFileName(Applicant applicant)
+        {
+            string uniqueFileName = null;
+
+            if (applicant.ProfilePhoto != null)
+            {
+                string uploadsFolder = Path.Combine(_webHost.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + applicant.ProfilePhoto.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                { 
+                    applicant.ProfilePhoto.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+
+
+        public IActionResult Details(int Id)
+        {
+            Applicant applicant=_context.Applicants
+                .Include(e =>  e.Experiences)
+                .Where(a => a.Id==Id).FirstOrDefault();
+            
+            return View(applicant);
+                
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            Applicant applicant = _context.Applicants
+                .Include(e => e.Experiences)
+                .Where(a => a.Id == Id).FirstOrDefault();
+
+            return View(applicant);
+
+        }
+        [HttpPost]
+        public IActionResult Delete(Applicant applicant)
+        {
+            _context.Attach(applicant);
+            _context.Entry(applicant).State = EntityState.Deleted;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+     
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            Applicant applicant = _context.Applicants
+                .Include(e => e.Experiences)
+                .Where(a => a.Id == Id).FirstOrDefault();
+
+            return View(applicant);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Applicant applicant)
+        {
+            _context.Attach(applicant);
+            _context.Entry(applicant).State = EntityState.Modified;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
+
+
